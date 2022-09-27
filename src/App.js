20a04185a09ff488ae4b303f1bb20b5bb3d41627
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-09-27 09:33:02 -0300

    final style touches

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
     <div className="App">
       <Banner />
       <Form fields={fields.map(field => field.name)} whenSubmited={company => whenNewCompany(company)}/>
-
+      <h1 style={{ textAlign: 'center', color: 'black', margin: '50px 0' }}>Clients:</h1>
       {fields.map(field => <Field
                               key={field.name}
                               name={field.name}
